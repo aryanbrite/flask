6a02916047aea214e36bb37041194bca582ae514
aryanbrite <aryanbrite@gmail.com>
2026-07-28 08:47:28 +0530

    added notes fprm

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask import request
 
 app=Flask(__name__)
 
+notes = []
+
+
 @app.route("/")
 def home():
-    title = "notes app"
-    return render_template("index.html", title=title)
+    title = "Notes"
+    return render_template("index.html", title=title, notes=notes)
+
+@app.route("/add", methods=["POST"])
+def add():
+    ho = request.form["note"]
+    notes.append(ho)
+    return redirect(url_for('home'))
+
 if __name__ == "__main__":
     app.run(debug=True)
 
-@app.route("/app", methods=["POST"])
-def add():
-    request.form["note"]
